65e4059b9c2e334f2bd3a19e1c652fa661817401
kophonegap <niconi@coca-cola.com>
2017-06-20 19:13:56 +0800

    ok

---
+++ www/project.js
@@ -0,0 +1,82 @@
+// JSON file describing the AR-experience for offline usage
+var projectJSONOffline =
+{
+    "isCloud": false,
+    "tcId": "5935a28eb2a567ec2ccb35e1",
+    "name": "Coke Love",
+    "wtcFiles": [
+        {
+            "url": "augmentation-trackers/4.1/tracker.wtc",
+            "nrOfTargets": 1,
+            "version": "4.1",
+            "creDat": 1496687276494
+        },
+        {
+            "url": "augmentation-trackers/4.1/tracker.wtc",
+            "nrOfTargets": 1,
+            "version": "5.0",
+            "creDat": 1496687276494
+        },
+        {
+            "url": "augmentation-trackers/4.1/tracker.wtc",
+            "nrOfTargets": 1,
+            "version": "5.1",
+            "creDat": 1496687276494
+        },
+        {
+            "url": "augmentation-trackers/4.1/tracker.wtc",
+            "nrOfTargets": 1,
+            "version": "5.2",
+            "creDat": 1496687276494
+        },
+        {
+            "url": "augmentation-trackers/4.1/tracker.wtc",
+            "nrOfTargets": 1,
+            "version": "5.3",
+            "creDat": 1496687276494
+        }
+    ],
+    "targets": [
+        {
+            "name": "together-love",
+            "augmentations": [
+                {
+                    "type": "ImageDrawable",
+                    "height": 0.3,
+                    "uri": "augmentation-assets/1192592603_together_love/5935a2c3eaee0d126326a54e/image.png",
+                    "targetId": "5935a2a558279f0d6318c271",
+                    "modDat": 1496687341500,
+                    "creDat": 1496687299568,
+                    "properties": {
+                        "name": "together-love",
+                        "forceNativeBrowser": "false",
+                        "ignoreInAR": false,
+                        "opacity": 1,
+                        "rotate": {
+                            "x": 0,
+                            "y": 0,
+                            "z": 0
+                        },
+                        "scale": {
+                            "x": 3.1145243949984676,
+                            "y": 3.1145243949984676,
+                            "z": 3.1145243949984676
+                        },
+                        "translate": {
+                            "x": 0,
+                            "y": 0,
+                            "z": 0.1050093844235
+                        },
+                        "zOrder": 0
+                    },
+                    "fileSize": 136741,
+                    "id": "5935a2c3eaee0d126326a54e"
+                }
+            ]
+        }
+    ],
+    "settings": {
+        "tracker": {}
+    }
+}
+;
